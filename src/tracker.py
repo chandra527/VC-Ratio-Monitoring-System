b9467e66c15859c1a4499d3b9fbb29e631e8c7fc
Eggi Chandra <e2gichandra@gmail.com>
2026-07-30 03:58:30 +0700

    Refactor tracker configuration

--- src/tracker.py
+++ src/tracker.py
@@ -1,16 +1,29 @@
+from config import CONFIDENCE, IMAGE_SIZE
 from yolo_detector import model
 
 
+VEHICLE_CLASSES = [2, 3, 5, 7]
+
+
 def track(frame):
+    """
+    Menjalankan deteksi dan tracking kendaraan pada satu frame.
+
+    Class YOLO yang diproses:
+    2 = car
+    3 = motorcycle
+    5 = bus
+    7 = truck
+    """
 
     results = model.track(
         frame,
         persist=True,
         tracker="bytetrack.yaml",
         verbose=False,
-        imgsz=960,
-        conf=0.15,
-        classes=[2, 3, 5, 7]
+        imgsz=IMAGE_SIZE,
+        conf=CONFIDENCE,
+        classes=VEHICLE_CLASSES,
     )
 
     return results[0]
